b21f337b04f914ee834fcbd8ccea57705f4692ec
vagrant <bahar.hosseini@gmail.com>
2022-08-17 23:52:13 +0000

    Instruction

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,9 +5,27 @@
  */
 
 
-// const createTweetElement = (tweetObj) =>{
+const createTweetElement = (tweetObj) =>{
+  let $tweet = (` <section class="tweet-container">
+    <article id="tweet">
+    <div class="userInfo">
+  <h3>
+    <img src=${tweetObj.user.avatars}></img>
+    ${tweetObj.user.name}
+  </h3>
+  <a>${tweetObj.user.handle}</a>
+</div>
+  <p>${tweetObj.content.text}</p>
+  <div class="font-awesome">
+    <i class="fa-solid fa-flag fa-2xs"></i>
+    <i class="fa-solid fa-repeat fa-2xs"></i>
+    <i class="fa-solid fa-heart fa-2xs"></i>
+  </div>
+</article>
+</section>`);
 
-// };
+  return $tweet;
+};
 
 const tweetData = {
   "user": {
@@ -21,31 +39,31 @@ const tweetData = {
   "created_at": 1461116232227
 };
 
-// const $tweet = createTweetElement(tweetData);
-const $tweet = $(` <section class="tweet-container">
-<ul>
-  <li>
-    <article id="tweet">
-    <div class="userInfo">
-  <h3>
-    <img src='https://i.imgur.com/73hZDYK.png'></img>
-    Bahar Hosseini
-  </h3>
-  
-  <a>@irIsaac</a>
-</div>
-  <p>Test new Tweet</p>
-  <div class="font-awesome">
-    <i class="fa-solid fa-flag fa-2xs"></i>
-    <i class="fa-solid fa-repeat fa-2xs"></i>
-    <i class="fa-solid fa-heart fa-2xs"></i>
-  </div>
-</article>
-</li>
-</ul>
-</section>`);
+
+
+
+const $tweet = createTweetElement(tweetData);
+
+
+// const $tweet = $(` <section class="tweet-container">
+//     <article id="tweet">
+//     <div class="userInfo">
+//   <h3>
+//     <img src='https://i.imgur.com/73hZDYK.png'></img>
+//     Bahar Hosseini
+//   </h3>
+//   <a>@irIsaac</a>
+// </div>
+//   <p>Test new Tweet</p>
+//   <div class="font-awesome">
+//     <i class="fa-solid fa-flag fa-2xs"></i>
+//     <i class="fa-solid fa-repeat fa-2xs"></i>
+//     <i class="fa-solid fa-heart fa-2xs"></i>
+//   </div>
+// </article>
+// </section>`);
 
 // Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
+// console.log($tweet); // to see what it looks like
 $('.main-container').prepend($tweet);
 
