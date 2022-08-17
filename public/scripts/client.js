15138521aa84fad896ca95b419d4a31c8fec0292
vagrant <bahar.hosseini@gmail.com>
2022-08-17 22:32:31 +0000

    fix some css bugs

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,6 +4,7 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
 // const createTweetElement = (tweetObj) =>{
 
 // };
@@ -20,8 +21,31 @@ const tweetData = {
   "created_at": 1461116232227
 };
 
-const $tweet = createTweetElement(tweetData);
+// const $tweet = createTweetElement(tweetData);
+const $tweet = $(` <section class="tweet-container">
+<ul>
+  <li>
+    <article id="tweet">
+    <div class="userInfo">
+  <h3>
+    <img src='https://i.imgur.com/73hZDYK.png'></img>
+    Bahar Hosseini
+  </h3>
+  
+  <a>@irIsaac</a>
+</div>
+  <p>Test new Tweet</p>
+  <div class="font-awesome">
+    <i class="fa-solid fa-flag fa-2xs"></i>
+    <i class="fa-solid fa-repeat fa-2xs"></i>
+    <i class="fa-solid fa-heart fa-2xs"></i>
+  </div>
+</article>
+</li>
+</ul>
+</section>`);
 
 // Test / driver code (temporary)
 console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet);
+$('.main-container').prepend($tweet);
+
