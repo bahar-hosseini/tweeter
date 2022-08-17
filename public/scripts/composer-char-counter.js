0874f949f44f45672e979b3d8dcea3f360f39fba
vagrant <bahar.hosseini@gmail.com>
2022-08-17 00:13:18 +0000

    set up the page and character count

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+  let numberOfchar = 0;
+
+  const $input = $('#tweet-text');
+  const $counter = $('.counter');
+
+  $input.keypress(function() {
+
+    $counter.text(numberOfchar += 1);
+    numberOfchar > 140 ? $counter.css('color','red') : null;
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const $button = $('button');
+// $button.on('click',(e)=>{
+//   console.log('i got clicked');
+
+//   const $input = $('#tweet-text');
+//   const value = $input.val();
+//   const $li = $(`<li>${value}</li>`).text(value);
+//   const $mainList = $(`main`);
+//   $mainList.prepend($li);
+//   $input.val('');
+//   $input.focus();
+// });
