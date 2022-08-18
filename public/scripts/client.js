2de5f06e7d326bc307b4406a6d0925f35654907b
vagrant <bahar.hosseini@gmail.com>
2022-08-18 14:08:06 +0000

    Add escape function to prevent xxs

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,17 +8,26 @@
 /**
  * A Function to create each tweet
  **/
+
+
 const createTweetElement = (tweetObj) =>{
+// escape function to prevent xxs
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
   let $tweet = (` <section class="tweet-container">
     <article id="tweet">
     <div class="userInfo">
   <h3>
-    <img src=${tweetObj.user.avatars}></img>
-    ${tweetObj.user.name}
+    <img src=${escape(tweetObj.user.avatars)}></img>
+    ${escape(tweetObj.user.name)}
   </h3>
-  <a>${tweetObj.user.handle}</a>
+  <a>${escape(tweetObj.user.handle)}</a>
 </div>
-  <p>${tweetObj.content.text}</p>
+  <p>${escape(tweetObj.content.text)}</p>
   <div class="font-awesome">
     <i class="fa-solid fa-flag fa-2xs"></i>
     <i class="fa-solid fa-repeat fa-2xs"></i>
